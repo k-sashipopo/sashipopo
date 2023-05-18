4cdb810813d57404030ec5bba3eb01f323cf1d37
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2023-05-18 09:36:34 +0000

    item/new 機能追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,18 +3,30 @@ class Admin::ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+    @genre = Genre.all
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item)
   end
 
   def show
+    @item = Item.find(params[:id])
+
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :intro, :tax_free_price, :sale_status, :genre_id, :image)
   end
 
 end
