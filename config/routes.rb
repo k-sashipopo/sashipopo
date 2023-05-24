f1ea2d4c8ae74b1a02ace701bf760db0f304e6b5
EC2 Default User <ec2-user@ip-172-31-17-93.ap-northeast-1.compute.internal>
2023-05-24 07:46:26 +0000

    注文詳細画面

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
 
     delete 'cart_items/destroy_all', to: 'cart_items#destroy_all', as: 'destroy_all_cart_items'
     resources :cart_items, only: [:index, :update, :destroy, :create]
-    
+
     get 'customers/:id/confirm' => 'customers#confirm', as: 'confirm'
     patch 'customers/:id/resign' => 'customers#resign', as: 'resign'
     resources :customers, only: [:show, :edit, :update]
@@ -43,8 +43,8 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     get 'homes/top'
 
     resources :orders, only: [:show, :update]
-    
-    patch 'order_details/update'
+
+    resources :order_details, only: [:update]
 
     resources :customers, only: [:index, :show, :edit, :update]
 
