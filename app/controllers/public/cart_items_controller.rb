00dcabdfb47ce4654d897dee4bed9ccf802991c9
RinaOta <127483650+gxxdrina@users.noreply.github.com>
2023-05-25 07:46:20 +0000

    全体のレイアウト調整

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,6 @@
 class Public::CartItemsController < ApplicationController
+  before_action :authenticate_customer!
+
   def index
     @cart_items = current_customer.cart_items.all
     @total_price = calculate_total_price(current_customer)
